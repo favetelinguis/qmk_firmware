a8ced58caac8fa6b3dd526f481d076c0da0ee947
Henrik Larsson <reg@henkelarsson.se>
2019-10-19 15:30:33 +0200

    change timing to 145

--- keyboards/crkbd/keymaps/favetelinguis/config.h
+++ keyboards/crkbd/keymaps/favetelinguis/config.h
@@ -45,8 +45,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #endif
 
 // Below are my changes
-#define AUTO_SHIFT_TIMEOUT 150
+#define AUTO_SHIFT_TIMEOUT 145
 #define NO_AUTO_SHIFT_ALPHA
 
-#define TAPPING_TERM 175 // For ctrl/alt dual key timeout
+#define TAPPING_TERM 145 // For ctrl/alt dual key timeout
 #define IGNORE_MOD_TAP_INTERRUPT
